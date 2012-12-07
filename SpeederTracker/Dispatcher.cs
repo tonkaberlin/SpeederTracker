c1023d9a3197965576111164e152d3f9acf0ffc9
Brian Farrell <bagpipes.brian@gmail.com>
2012-12-06 21:43:25 -0500

    Basic Structure for Dispatcher Outlined

---
+++ SpeederTracker/Dispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+public class Dispatcher
+{
+    public Dispatcher()
+	{
+        public Dispatcher()
+        {
+            //Create several cars
+            ;
+        }
+
+        public void NotifySpeeder(double cameralocation, double speed, File image)
+        {
+
+        }
+	}
+}
---
+++ SpeederTracker/SpeederTracker/Dispatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpeederTracker
+{
+    class Dispatcher
+    {
+        List<Car> cars = new List<Car>();
+        Random r = new Random();
+        public Dispatcher()
+        {
+            for(int i = 0; i < 3; i++)
+                cars.Add(new Car(r.Next(0, 1000)));
+        }
+
+        public void NotifySpeeder(int cameralocation, double speed, File image)
+        {
+
+        }
+
+        public void StoreInDatabase()
+        {
+            //Call the method to store stuff in the database
+        }
+
+        public void NotifyCar(int cameralocation, double speed, File image)
+        {
+            Car c = cars.OrderBy(x => x.Location - cameralocation).First();
+            c.speeder(cameralocation, speed, image);
+        }
+    }
+}
